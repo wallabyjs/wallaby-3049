046a082a7636040dac4896c8120de5fe1a87d563
bhvgt <bhavin.kamani@gembatech.com>
2022-08-14 21:46:44 +0530

    globalThis does not work

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 1. git clone git@github.com:bhvngt/wallabyjs-pnpm-workspace-vitest.git
 2. cd wallabyjs-pnpm-workspace-vitest
 3. pnpm install
+4. cd libs/foo
+5. vitest
--- libs/foo/test/setupTest.ts
+++ libs/foo/test/setupTest.ts
@@ -1,6 +1,2 @@
-import { beforeAll } from 'vitest'
 import { webcrypto } from "node:crypto";
-
-beforeAll(() => {
-    globalThis.crypto = webcrypto as Crypto;
-})
+globalThis.crypto = webcrypto as Crypto;
